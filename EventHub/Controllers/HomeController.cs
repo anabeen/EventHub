924760f324c7415fe960642b47e3ac45bcf2952d
Nabeen <axnabeen@gmail.com>
2021-03-04 13:47:56 -0500

    Added upcoming events Added Name for dropdownMenu Added Name when registering

--- EventHub/Controllers/HomeController.cs
+++ EventHub/Controllers/HomeController.cs
@@ -1,16 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EventHub.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace EventHub.Controllers
 {
+
+
+
     public class HomeController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingEvents = _context.Events
+                                .Include(g => g.Planner)
+                                .Include(g => g.EventTheme)
+                                .Where(m => m.DateTime > DateTime.Now);
+            return View(upcomingEvents);
         }
 
         public ActionResult About()
